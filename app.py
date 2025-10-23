5679bf062ecd30917b4e863cb04ad8b3f83bf363
devstephenke-sys <devstephen.ke@gmail.com>
2025-10-23 09:48:50 +0300

    Update app.py

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ st.markdown(f"""
         </div>
         <div style="text-align:right;">
             <p style="margin:0; font-size:14px; color:#ddd;">Generated on {today}</p>
-            <img src="DonBoscoTechAfricaLogo.png" width="120" style="border-radius:8px; margin-top:6px;">
+            <img src="https://tvet.dbtechafrica.org/pluginfile.php/1/core_admin/logo/0x200/1747917709/LogoRGB%20%283%29.png" width="120" style="border-radius:8px; margin-top:6px;">
         </div>
     </div>
 </div>
